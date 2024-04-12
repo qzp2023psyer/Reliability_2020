612dc49f290493b2883b2bcc44ba3a523d355aca
qzp2023psyer <qzp2023psyer@163.com>
2024-04-12 20:49:02 +0800

    this is file about installing rstan and cmdstan

---
+++ install rstan.R
@@ -0,0 +1,10 @@
+#在开始之前需要下载rtool,配置相应的环境变量
+install.packages("remotes")
+remotes::install_github("stan-dev/cmdstanr")
+library(cmdstanr)
+#set_cmdstan_path("C:\RTools43\usr\bin")
+#C:\RTools\RTools40\usr\bin
+install.packages("devtools")
+devtools::install_github("stan-dev/cmdstanr")
+library(cmdstanr)
+install_cmdstan()
